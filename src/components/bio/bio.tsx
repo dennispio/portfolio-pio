b46da09b6d9d530e578ddbb4b3a775a4d68d19b6
dennispio <dennis.piotrowski@web.de>
2019-10-01 16:34:37 +0200

    try1

--- src/components/bio/bio.tsx
+++ src/components/bio/bio.tsx
@@ -15,26 +15,26 @@ const Bio = (): JSX.Element => {
         </div>
       </div>
       <div className="flex flex-wrap md:flex-no-wrap fade-in-unten">
-        <div className="flex-direction w-1/2">
+        <div className="flex-direction w-1/2 md:w-1/4">
           <div className="p-1 text-lg text-grey1 typewriter-text">FRONTEND</div>
           <div className="p-1 text-sm">JAVASCRIPT</div>
           <div className="p-1 text-sm ">TYPESCRIPT</div>
           <div className="p-1 text-sm">REACTJS</div>
           <div className="p-1 text-sm">GATSBYJS</div>
         </div>
-        <div className="flex-direction w-1/2">
+        <div className="flex-direction w-1/2 md:w-1/4">
           <div className="p-1 text-lg text-grey1">BACKEND</div>
           <div className="p-1 text-sm">JAVA</div>
           <div className="p-1 text-sm">NODEJS</div>
           <div className="p-1 text-sm ">EXPRESSJS</div>
         </div>
-        <div className="flex-direction w-1/2">
+        <div className="flex-direction w-1/2 md:w-1/4">
           <div className="p-1 text-lg text-grey1">WEITERES</div>
           <div className="p-1 text-sm">SCRUM</div>
           <div className="p-1 text-sm">USABILITY</div>
           <div className="p-1 text-sm">UX DESIGN</div>
         </div>
-        <div className="flex-direction w-1/2">
+        <div className="flex-direction w-1/2 md:w-1/4">
           <div className="p-1  text-lg text-grey1">MARKETING</div>
           <div className="p-1  text-sm">ONPAGE SEO</div>
           <div className="p-1  text-sm">OFFPAGE SEO</div>
