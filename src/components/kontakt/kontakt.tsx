ef21ad1477445543702cd3bd1c7fc858b339fb48
dennispio <dennis.piotrowski@web.de>
2020-02-13 22:34:14 +0100

    post

--- src/components/kontakt/kontakt.tsx
+++ src/components/kontakt/kontakt.tsx
@@ -21,6 +21,7 @@ const Kontakt = (): JSX.Element => {
             data-netlify="true"
             data-netlify-honeypot="bot-field"
             name="contactForm"
+            encType="application/x-www-form-urlencoded"
           >
             <input type="hidden" name="bot-field" />
             <input type="hidden" name="form-name" value="contactForm" />
