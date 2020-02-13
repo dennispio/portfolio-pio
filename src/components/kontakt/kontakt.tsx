f8f89cb03e09b7278574e426c458617d47c0a7c8
dennispio <dennis.piotrowski@web.de>
2020-02-13 11:31:16 +0100

    kontakt

--- src/components/kontakt/kontakt.tsx
+++ src/components/kontakt/kontakt.tsx
@@ -20,19 +20,27 @@ const Kontakt = (): JSX.Element => {
             method="POST"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
-            name="kontakt"
+            name="contact"
           >
             <input type="hidden" name="bot-field" />
-            <input type="hidden" name="form-name" value="kontakt" />
-            <label className="text-grey1">NAME</label>
-            <input className="text-black h-8" required name="Name" type="text" id="Name" />
-            <label className="text-grey1 pt-3">E-MAIL</label>
-            <input className="text-black  h-8" required name="Email" type="email" id="Email" />
-            <label className="text-grey1 pt-3">PHONE</label>
-            <input className="text-black  h-8" required name="Phone" id="Phone" />
-            <label className="text-grey1 pt-3">MESSAGE</label>
-            <textarea className="text-black  h-24" name="Nachricht" id="Nachricht" required />
-            <label className="pt-5">
+            <input type="hidden" name="form-name" value="contact" />
+            <label className="text-grey1" htmlFor="fname">
+              NAME
+            </label>
+            <input className="text-black h-8" required name="name" type="text" id="name" />
+            <label className="text-grey1 pt-3" htmlFor="femail">
+              E-MAIL
+            </label>
+            <input className="text-black  h-8" required name="email" type="email" id="email" />
+            <label className="text-grey1 pt-3" htmlFor="fphone">
+              PHONE
+            </label>
+            <input className="text-black  h-8" required name="phone" id="phone" />
+            <label className="text-grey1 pt-3" htmlFor="fmessage">
+              MESSAGE
+            </label>
+            <textarea className="text-black  h-24" name="nachricht" id="nachricht" required />
+            <label className="pt-5" htmlFor="fprivacy">
               <Link to="legal/de/privacy" target="_blank" className="pr-5">
                 PRIVACY
               </Link>{' '}
